464ce6265b9b9cbfd743aa02e31e6e1e4b09179b
Khadiza <sayma.khadiza@gmail.com>
2024-09-24 22:27:39 +0600

    3 card done

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Card-1
 document.getElementById('btn').addEventListener('click',function(e){
     e.preventDefault();
     const amount=getinputbyId('donateval');
@@ -5,8 +6,11 @@ document.getElementById('btn').addEventListener('click',function(e){
     const totdonateval=getinputbyText('donation');
     if(isNaN(amount) || amount>balance || amount==""){
         alert('Invalid input');
+        document.getElementById('donateval').value=' ';
         return;
     }
+    else {
+    document.getElementById('my_modal_5').showModal(); 
     document.getElementById('donation').innerText=amount+totdonateval;
     const newbalance=balance-amount;
     document.getElementById('personal-balance').innerText=newbalance;
@@ -21,5 +25,67 @@ document.getElementById('btn').addEventListener('click',function(e){
     `
     document.getElementById('history').appendChild(div);
     document.getElementById('donateval').value=' ';
+    }
+})
+
+//Card-2
+document.getElementById('btn-2').addEventListener('click',function(e){
+    e.preventDefault();
+    const amount=getinputbyId('donateval2');
+    const balance=getinputbyText('personal-balance');
+    const totdonateval=getinputbyText('donation2');
+    if(isNaN(amount) || amount>balance || amount==""){
+        alert('Invalid input');
+        document.getElementById('donateval2').value=' ';
+        return;
+    }
+    else{
+    document.getElementById('my_modal_5').showModal(); 
+    document.getElementById('donation2').innerText=amount+totdonateval;
+    const newbalance=balance-amount;
+    document.getElementById('personal-balance').innerText=newbalance;
+    
+    // history upadate
+    const name=document.getElementById('donate-name2').innerText;
+    const date=new Date();
+    const div=document.createElement('div');
+    div.innerHTML=`
+    <h2 class="text-fontMain font-bold">${amount} Taka is ${name}</h2>
+        <p>Date: ${date}</p>
+    `
+    document.getElementById('history').appendChild(div);
+    document.getElementById('donateval2').value=' ';
+    }
+})
+
+//Card-3
+document.getElementById('btn-3').addEventListener('click',function(e){
+    e.preventDefault();
+    const amount=getinputbyId('donateval3');
+    const balance=getinputbyText('personal-balance');
+    const totdonateval=getinputbyText('donation3');
+    if(isNaN(amount) || amount>balance || amount==""){
+        alert('Invalid input');
+        document.getElementById('donateval3').value=' ';
+        return;
+    }
+    else{
+    document.getElementById('my_modal_5').showModal(); 
+    document.getElementById('donation3').innerText=amount+totdonateval;
+    const newbalance=balance-amount;
+    document.getElementById('personal-balance').innerText=newbalance;
+    
+    // history upadate
+    const name=document.getElementById('donate-name3').innerText;
+    const date=new Date();
+    const div=document.createElement('div');
+    div.innerHTML=`
+    <h2 class="text-fontMain font-bold">${amount} Taka is ${name}</h2>
+        <p>Date: ${date}</p>
+    `
+    document.getElementById('history').appendChild(div);
+    document.getElementById('donateval3').value=' ';
+    }
+    
 })
 
